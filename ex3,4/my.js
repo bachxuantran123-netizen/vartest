2091908a74c649b766faf1c455231cdf234dd41f
bachtran <bachxuantran123@gmail.com>
2025-09-02 13:17:12 +0700

    Add files via upload

---
+++ ex3,4/my.js
@@ -0,0 +1,6 @@
+let a = +prompt("Nhap ban kinh hinh tron: ");
+let b = 3.14
+let S = a * a * b
+let V = 2 * b * a
+document.write("Dien tich hinh tron la: " + S);
+document.write("Chu vi hinh tron la: " + V);
